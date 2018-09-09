a2c6d62191eb09187ba450ef58dbb56dd2414ac8
yukito ohira <yukito.sys.so@gmail.com>
2018-09-10 00:08:08 +0900

    package.json

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import Vuex from 'vuex'
 
 Vue.config.productionTip = false
+
+Vue.use(Vuex)
 
 /* eslint-disable no-new */
 new Vue({
