bdf42f000fa4445a513c8861a8406932dccab4e7
yukito ohira <yukito.sys.so@gmail.com>
2018-09-10 12:24:51 +0900

    商品情報詳細画面

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,15 +1,38 @@
 <template>
-  <div class="product">
+  <div class="product" v-if="item" key="product">
     <h1>商品情報</h1>
-    <p>このページは、ID.{{ $route.params.id }} の詳細を表示する</p>
+    <dl class="product-table">
+      <dt>商品名</dt>
+      <dd>{{item.name}}</dd>
+      <dt>価格</dt>
+      <dd>{{item.price}}</dd>
+      <dt>商品説明</dt>
+      <dd>{{item.content}}</dd>
+    </dl>
   </div>
-
+  <div v-else key="loading">商品情報を読み込んでいます...</div>
 </template>
 
 <script>
+  import products from '@/api/products.js'
+  
   export default {
     name: "Product",
-    props: {id: Number}
+    props: {id: Number},
+    data() {
+      return {
+        item: null
+      }
+    },
+    watch: {
+      id: {
+        handler() {
+          products.asyncFind(this.id, item => {
+            this.item = item
+          })
+        }, immediate: true
+      }
+    }
   }
 </script>
 
